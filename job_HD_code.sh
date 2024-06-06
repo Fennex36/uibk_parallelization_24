56bcc7d1ac79df846ffab8e0e14efa29e28c1639
LV 703078 PS Parallele Programmierung <cb761156@login.lcc3.uibk.ac.at>
2024-06-06 13:44:00 +0200

    added dedicated job scripts for assignments

--- job_HD_code.sh
+++ job_HD_code.sh
@@ -17,5 +17,4 @@
 
 
 module load openmpi/3.1.6-gcc-12.2.0-d2gmn55 gcc/12.2.0-gcc-8.5.0-p4pe45v
-# mpiexec -n $SLURM_NTASKS assignments/02/build/mandelbrot_mpi
 mpiexec -n $SLURM_NTASKS code_students/build/apps/run_full_code_parallel
